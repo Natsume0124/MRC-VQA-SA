f482f4fc2f903439e62e23f369da515e8d062e61
He RuiMing <116568044+Natsume0124@users.noreply.github.com>
2025-05-28 20:05:28 +0800

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -74,10 +74,12 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     output = {}
     if phase_codename == "VG-RS":
         print("Evaluating for VG-RS Phase")
-        with open(test_annotation_file, 'r') as f:
-            user_data = json.load(f)
-        with open(user_submission_file, 'r') as f:
+    # 读取JSON文件
+        with open(test_annotation_file, 'r', encoding='utf-8') as f:
             test_data = json.load(f)
+    
+        with open(user_submission_file, 'r', encoding='utf-8') as f:
+            user_data = json.load(f)
             # 构建用户提交数据的查找字典
         user_dict = {(item['image_path'], item['question']): item for item in user_data}
     
