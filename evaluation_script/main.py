ae6629f7a801f958617b8e5126ff9199eda91aad
He RuiMing <116568044+Natsume0124@users.noreply.github.com>
2025-05-29 17:44:06 +0800

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -3,6 +3,52 @@ import json
 # from zhipuai import ZhipuAI
 import requests
 from requests.exceptions import ConnectionError, Timeout
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+def install(package):
+    # Install a pip python package
+
+    # Args:
+    #     package ([str]): Package name with version
+    try:
+        subprocess.run([sys.executable,"-m","pip","install",package])
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while installing {package}: {e.stderr}")
+    except FileNotFoundError:
+        print("Error: Pip is not found. make sure you have pip installed.")
+    except PermissionError:
+        print("Error: Permission denied. ")
+
+
+def install_local_package(folder_name):
+    # Install a local python package
+
+    # Args:
+    #     folder_name ([str]): name of the folder placed in evaluation_script/
+    
+    
+    try:
+        subprocess.run([
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            os.path.join(str(Path(__file__).parent.absolute()), folder_name)
+        ], capture_output=True, text=True, check=True)
+        print(f"Successfully installed local package from {folder_name}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while installing local package from {folder_name}: {e.stderr}")
+    except FileNotFoundError:
+        print("Error: Pip not found.")
+    except PermissionError:
+        print("Error: Permission denied. ")
+
+# install("shapely==1.7.1")
+# install("requests==2.25.1")
+
 def check_zhipuai_import():
     """
     检查 zhipuai 包和 ZhipuAI 类是否导入成功
@@ -152,6 +198,8 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
             'submitted_at': u'2017-03-20T19:22:03.880652Z'
         }
     """
+    install("zhipuai")
+    install_local_package("package_folder_name")
     net2 = test_zhipuai_import()
     net = None
     if check_internet_connection():
