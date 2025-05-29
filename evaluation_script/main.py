abb098c8258f1a71bab770fc61b36e9609458458
He RuiMing <116568044+Natsume0124@users.noreply.github.com>
2025-05-29 14:56:01 +0800

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -106,7 +106,7 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     net = None
     if check_internet_connection():
         print("网络状态: 已连接国际互联网")
-        net = 109029890808555
+        net = 109029890808555666
     else:
         print("网络状态: 无法连接国际互联网")
         net = 3556211
